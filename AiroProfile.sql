2aba19f4c3f28be99fc1b2413b2693801a00d951
Sagar R Bandagar <sagarbandagar18@gmail.com>
2024-06-20 13:52:37 +0530

    Added the database file

---
+++ AiroProfile.sql
@@ -0,0 +1,23 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS `AiroProfile` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+-- Use the created database
+USE `AiroProfile`;
+
+-- Create the table if it doesn't exist
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `organisation` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(100) NOT NULL,
+  `state` varchar(100) NOT NULL,
+  `country` varchar(100) NOT NULL,
+  `postalcode` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+-- Select all records from the accounts table
+SELECT * FROM `AiroProfile`.`accounts`;
